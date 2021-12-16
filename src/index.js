391617ee9934d8bb170aa0de413bc50bafa38111
pblgllgs <pbl.gllgs@gmail.com>
2021-12-16 13:17:47 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FocusScrreen } from './components/04-useRef/FocusScrreen';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import CounterApp from './components/01-useState/CounterApp';
 // import HooksApp from './HooksApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScrreen } from './components/04-useRef/FocusScrreen';
 
 ReactDOM.render(
     <FocusScrreen />,
