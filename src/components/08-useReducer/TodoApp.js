af96007303092fd155febdb14d879f60b20382a1
pblgllgs <pbl.gllgs@gmail.com>
2021-12-17 17:28:56 -0300

    updates

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -45,7 +45,7 @@ export const TodoApp = () => {
 
     return (
         <div>
-            <h1>TodoApp ({todos.length})</h1>
+            <h1>TodoApp ({todos.length}) items</h1>
             <hr />
             <div className="row">
                 <div className="col-7">
