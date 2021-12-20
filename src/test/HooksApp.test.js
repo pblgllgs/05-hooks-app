f0bbead39f31b5bd41cb956bb6bbc2eb6cc8bd37
pblgllgs <pbl.gllgs@gmail.com>
2021-12-20 10:56:13 -0300

    snapshot hookapp

---
+++ src/test/HooksApp.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow } from "enzyme";
+import HooksApp from '../HooksApp'
+
+describe('Pruebas sobre HooksApp', () => {
+
+    let wrapper = shallow(<HooksApp />);
+
+    test('Debe de mostrar correctamente', () => {
+        jest.clearAllMocks();
+        expect(wrapper).toMatchSnapshot();
+    })
+    
+})
