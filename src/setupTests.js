9b1ef8ea5ff3ad0c24c412e3bc9ca41ce337016d
pblgllgs <pbl.gllgs@gmail.com>
2021-12-20 11:33:35 -0300

    test useCounter

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,6 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import {createSerializer} from 'enzyme-to-json';
-// import { renderHook } from '@testing-library/react-hooks'
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
--- src/test/hooks/useConter.test.js
+++ src/test/hooks/useConter.test.js
@@ -1,12 +1,51 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useCounter } from "../../hooks/useCounter";
 
+describe("Pruebas sobre useCounter", () => {
+    test("Debe de retornar valores por defecto", () => {
+        const { result } = renderHook(() => useCounter());
+        expect(result.current.counter).toBe(10);
+        expect(typeof result.current.increment).toBe("function");
+        expect(typeof result.current.decrement).toBe("function");
+        expect(typeof result.current.reset).toBe("function");
+    });
 
-describe('Pruebas sobre useCounter', () => {
-    test('Debe de retornar valores por defecto ', () => {
+    test("Debe de tener el counter en 100", () => {
+        const { result } = renderHook(() => useCounter(100));
+        expect(result.current.counter).toBe(100);
+    });
 
-        const {re} = renderHook( ()=>  useCounter());
+    test("Debe de incrementar el counter en 1", () => {
+        const { result } = renderHook(() => useCounter());
+        const { increment } = result.current;
+        //ejecucion de funciones dentro del cuerpo de un test
+        act(() => {
+            increment();
+        });
+        const { counter } = result.current;
+        expect(counter).toBe(11);
+    });
 
-    })
-    
-})
+    test("Debe de decrementar el counter en 1", () => {
+        const { result } = renderHook(() => useCounter());
+        const { decrement } = result.current;
+        //ejecucion de funciones dentro del cuerpo de un test
+        act(() => {
+            decrement();
+        });
+        const { counter } = result.current;
+        expect(counter).toBe(9);
+    });
+
+    test("Debe de dar reset", () => {
+        const { result } = renderHook(() => useCounter());
+        const { reset, increment } = result.current;
+        //ejecucion de funciones dentro del cuerpo de un test
+        act(() => {
+            increment();
+            reset();
+        });
+        const { counter } = result.current;
+        expect(counter).toBe(10);
+    });
+});
