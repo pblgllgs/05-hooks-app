81ba80b59f80d73b907b2f7ded4641c8114543aa
pblgllgs <pbl.gllgs@gmail.com>
2021-12-20 17:07:36 -0300

    sttimeout jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,5 +6,7 @@ import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import {createSerializer} from 'enzyme-to-json';
 
+jest.setTimeout(30000);
+
 Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
